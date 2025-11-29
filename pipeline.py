4c913d753de9eb9fa6468cd7893fe3ab3f0e84cc
i220493 <i220493@nu.edu.pk>
2025-11-29 18:18:30 +0500

    Track model.pkl with DVC and update pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,19 +1,32 @@
+import mlflow
+import os
 from src.pipeline_components import fetch_data, preprocess_data, train_model, evaluate_model
 
 DATA_PATH = "data/raw_data.csv"
 MODEL_PATH = "data/processed/model.pkl"
 METRICS_PATH = "data/processed/metrics.json"
 
-# 1. Fetch data
-df = fetch_data(DATA_PATH)
+os.makedirs("data/processed", exist_ok=True)
 
-# 2. Preprocess data
-X_train, X_test, y_train, y_test, scaler = preprocess_data(df)
+# Start an MLflow run
+mlflow.set_experiment("housing_pipeline")
 
-# 3. Train model
-model = train_model(X_train, y_train, MODEL_PATH)
+with mlflow.start_run(run_name="run1"):
 
-# 4. Evaluate model
-metrics = evaluate_model(MODEL_PATH, X_test, y_test, METRICS_PATH)
-
-print("Pipeline completed. Metrics:", metrics)
+    # 1. Fetch data
+    df = fetch_data(DATA_PATH)
+    
+    # 2. Preprocess data
+    X_train, X_test, y_train, y_test, scaler = preprocess_data(df)
+    
+    # 3. Train model
+    model = train_model(X_train, y_train, MODEL_PATH)
+    mlflow.sklearn.log_model(model, "model")
+    
+    # 4. Evaluate model
+    metrics = evaluate_model(MODEL_PATH, X_test, y_test, METRICS_PATH)
+    
+    # Log metrics to MLflow
+    mlflow.log_metrics(metrics)
+    
+    print("Pipeline completed. Metrics:", metrics)
